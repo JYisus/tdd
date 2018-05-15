9dc7028b795d0cfa0524f3d0deafb5a5ba86c738
Juan Jesús Padrón Hernández <alu0100976731@ull.edu.es>
2018-05-15 10:53:31 +0100

    Segundo caso: error

---
+++ c.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+function Raices(a,b,c)
+{
+	return [[5,0],[0,0]]
+}
+
+assert.deepEqual(Raices(1,-5,0), [[5,0],[0,0]]);
+assert.deepEqual(Raices(2,5,0), [[0,0],[2.5,0]]);
